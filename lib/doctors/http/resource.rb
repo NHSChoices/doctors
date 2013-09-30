70992d64fd11ca86af0b7c524117f5c222f3f44e
Russell <russell@russelldunphy.com>
2013-09-30 15:10:42 +0100

    Empty endpoints for GP search

--- lib/doctors/http/resource.rb
+++ lib/doctors/http/resource.rb
@@ -1,6 +1,13 @@
 module Doctors
   module Http
     module Resource
+
+      def resource(name, endpoint)
+      end
+
+      def resources(name, endpoint)
+      end
+
     end
   end
 end
---
+++ spec/lib/doctors/http/resource_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+module Doctors
+  module Http
+    describe Resource do
+    end
+  end
+end
