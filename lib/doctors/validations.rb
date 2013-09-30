50f114ff4dff6de59ef07558205d893f57fcddf4
Russell <russell@russelldunphy.com>
2013-09-30 16:17:41 +0100

    :clap: Working location search

--- lib/doctors/validations.rb
+++ lib/doctors/validations.rb
@@ -3,17 +3,12 @@ require 'delegate'
 module Doctors
   module Validations
 
-    def self.included(base)
-      base.extend(Descriptor)
-    end
-
-    module Descriptor
-      delegate :validate,              to: :form_object
-      delegate :validates,             to: :form_object
-      delegate :validates!,            to: :form_object
-      delegate :validates_each,        to: :form_object
-      delegate :validates_with,        to: :form_object
-    end
+    delegate :validate,              to: :form_object
+    delegate :validates,             to: :form_object
+    delegate :validates!,            to: :form_object
+    delegate :validates_each,        to: :form_object
+    delegate :validates_with,        to: :form_object
+    delegate :validates_presence_of, to: :form_object
   end
 end
 
