671e903842b09f9624d0405a87bdb426a0b6fccd
Russell <russell@russelldunphy.com>
2013-10-18 11:09:50 +0100

    eu

--- Gemfile
+++ Gemfile
@@ -1,2 +1,3 @@
 source 'https://rubygems.org'
 gemspec
+gem 'id', github: 'rsslldnphy/id', branch: '0.1'
--- spec/lib/doctors/search_spec.rb
+++ spec/lib/doctors/search_spec.rb
@@ -6,13 +6,13 @@ module Doctors
     let (:search) { Search.new(location: 'NR1') }
 
     it 'performs a practice search' do
-      stub_request(:get, "http://nhs-api.cloudapp.net/api/Organisations?limit=10&offset=0&organisationtype=GPB").to_return(Fixtures.practices)
+      stub_request(:get, "http://nhs-api.cloudapp.net/api/Organisations/NR1?limit=10&offset=0&organisationtype=GPB").to_return(Fixtures.practices)
       expect(search.results).to have_at_least(1).item
       search.results.each { |result| expect(result).to be_a Practice }
     end
 
     it 'raises an API::Error on a non-200 response' do
-      stub_request(:get, "http://nhs-api.cloudapp.net/api/Organisations?limit=10&offset=0&organisationtype=GPB").to_return(Fixtures.not_found)
+      stub_request(:get, "http://nhs-api.cloudapp.net/api/Organisations/NR1?limit=10&offset=0&organisationtype=GPB").to_return(Fixtures.not_found)
       expect { search.results }.to raise_error API::Error
     end
 
